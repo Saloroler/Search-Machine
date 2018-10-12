28b87651ca676e5c9970d3adb6fdb75dee4a917c
Oleg Solonina <osolonina@Ascendix.net>
2018-10-12 18:39:02 +0300

    first commit

---
+++ params.js
@@ -0,0 +1,77 @@
+var bodyP = {
+    fontFamily: 'Montserrat',
+    fontSize: '15px',
+    backgroundImage: 'url(gifStar.gif)',
+    backgroundPositionX: 'center',
+    backgroundSize: 'cover',
+    color: 'white'
+}
+var tableP = {
+    marginBottom: '10px',
+    width:'70%',
+    border:'1px solid white',
+    display:'block',
+    marginLeft:'auto',
+    marginRight:'auto',
+    borderCollapse:'collapse'
+}
+
+var cellP = {
+    borderRight:'1px solid white',
+    padding: '10px'
+}
+var cell12P = {
+    padding: '10px'
+}
+var rowP = {
+    borderBottom: '1px solid white'
+}
+
+
+
+var linkP = {
+    color: 'aliceblue'
+}
+
+
+                    ////////////////////////////////Header////////////////////////////
+
+var buttonDP = {
+    display: 'block',
+    fontSize: '1.17em',
+    fontWeight: 'bold',
+    color: 'white',
+    border: 'none',
+    fontFamily: 'montserrat',
+    background: 'transparent',
+    marginLeft: '40%',
+    cursor: 'pointer'
+}
+
+
+var div1P = {
+    display: 'flex',
+    justifyContent: 'space-evenly'
+}
+
+var inputFieldP = {
+    marginBottom: '20px',
+    marginTop: '15px',
+    display: 'block',
+    width: '200px',
+    backgroundColor: 'transparent',
+    marginLeft:'auto',
+    marginRight:'auto',
+    color: 'white',
+    fontSize: '15px',
+    fontFamily: 'montserrat',
+    fontWeight: '600'
+}
+
+var h1StartP = {
+    marginLeft:'auto',
+    marginRight:'auto',
+    width:'200px',
+    textAlign:'center',
+    marginTop: '255px',
+}
---
+++ training.js
@@ -0,0 +1,148 @@
+
+//  const newLocal = ((arr) => { var finalArr = [], result = arr.reduce((pre, curr) => { finalArr.push(pre + curr); return pre + curr; }, 0); return finalArr; })([1, 2, 3, 4, 5]);
+
+//  const newLocal = (arr => arr.reduce((sum, current, index, arr) => sum.push(arr.slice(0, index + 1).reduce((sum, current) => current + sum, 0)) && sum, []))([1, 2, 3, 4, 5]);
+
+var body = document.body;
+     makeStyle(body, bodyP);
+     var h1 = document.createElement('h1');
+     h1.appendChild(document.createTextNode('Search machine'))
+     makeStyle(h1, h1StartP);
+     body.appendChild(h1);
+
+  var form = document.createElement('form');
+
+     var inputField = document.createElement('input');
+     inputField.id = 'inputId';
+     inputField.autocomplete = 'off';
+     makeStyle(inputField, inputFieldP);
+     body.appendChild(inputField);
+
+
+// Input transference data
+//new event
+inputField.addEventListener('change', dataSearch);
+
+// Data transference to request
+function dataSearch(){
+    if(!inputField.value){
+        return console.log('Please fill the field correctly');
+    }
+    var div1 = document.createElement('div');
+    makeStyle(div1, div1P);
+
+    var h3 = document.createElement('h3');
+     h3.appendChild(document.createTextNode('Search - ' + inputField.value));
+     div1.appendChild(h3);
+
+     var buttonD = document.createElement('button');
+     buttonD.appendChild(document.createTextNode('Delete'))
+     makeStyle(buttonD, buttonDP);
+     div1.appendChild(buttonD);
+
+     buttonD.addEventListener("click", function(){
+         
+        body.removeChild(document.getElementById('tableId'));
+        div1.removeChild(buttonD);
+        div1.removeChild(h3);
+        if(document.getElementById('tableId') == null){
+            h1.style.marginTop = '255px';
+        }
+     })
+
+    body.appendChild(div1);
+
+     createRequest(inputField.value);
+     inputField.value = "";
+}
+
+// Create request
+function createRequest(question){
+    if(question) {
+        var key = "AIzaSyDcb7RF6xm_Wma4MFRfdPZ7GuNYy7ha0qI",
+            engineId = "011851895572920026093:og7uyxntu5g",
+                clientUrl = "https://www.googleapis.com/customsearch/v1";
+        var reqUrl = clientUrl + "?key=" + key + "&cx=" + engineId + "&q=" + question;
+       var result = httpGet(reqUrl);
+       var some = JSON.parse(result);
+       
+       var items = some.items;
+       var itemLinks = [];
+       var itemTitle = [];
+    for (var i = 0; i < items.length; i++) {
+        itemLinks.push(items[i].link);
+        itemTitle.push(items[i].title);
+    }
+   var arrForFront = [];
+   arrForFront.push(itemLinks);
+   arrForFront.push(itemTitle);
+ makeFront(arrForFront);
+    }
+}
+
+//getData from google api search
+function httpGet(url) {
+    var obj = {};
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, false);
+    xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var bodyReq = xhr.responseText;
+        obj = bodyReq;
+    } else {
+      console.error('Must be day limit of keysearch');
+    }
+};
+xhr.send();
+return obj;
+}
+
+
+// Create TABLE 
+ function makeFront(arr){
+//Before generation table change style input(to top)
+  h1.style.marginTop = '0';
+//
+
+
+var table = document.createElement("table");
+makeStyle(table, tableP);
+table.id = 'tableId';
+body.appendChild(table);
+    
+    for(var j = 0; j < arr[0].length; j++){
+    var row = document.createElement('tr');
+
+    var cell = document.createElement('td');
+    cell.appendChild(document.createTextNode(j));
+    makeStyle(cell, cellP);
+    row.appendChild(cell);
+
+        var cell1 = document.createElement('td');
+        cell1.appendChild(document.createTextNode(arr[1][j]));
+        makeStyle(cell1, cell12P);
+    row.appendChild(cell1);
+
+        var cell2 = document.createElement('td');
+            var link = document.createElement('a');
+            link.href = arr[0][j];
+            link.appendChild(document.createTextNode(arr[0][j].substring(0,150)));
+            makeStyle(link, linkP);
+        cell2.appendChild(link);
+        makeStyle(cell2, cell12P);
+    row.appendChild(cell2);
+
+    makeStyle(row, rowP);
+table.appendChild(row);
+    }
+    table.lastChild.style.border = 'none';
+    
+ }
+
+ //Styles
+ function makeStyle(element, params){
+     for(var key in params){
+        element.style[key] = params[key];
+     }
+ }
+
